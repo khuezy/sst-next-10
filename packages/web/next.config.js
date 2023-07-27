bcb3941c4b5c1789530c6b864edc4273f7d3c293
Khue Nguyen <khuezy.nguyen@gmail.com>
2023-07-26 19:58:10 -0700

    change mode to standalone

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  outputFileTracing: '.',
   experimental: {
     appDir: true,
     serverActions: true,
