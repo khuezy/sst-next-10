bb436e7a95a10313e3308bdb3479bb24defae029
Khue Nguyen <khuezy.nguyen@gmail.com>
2023-07-29 11:37:51 -0700

    add fetch call

--- packages/web/app/sa/page.tsx
+++ packages/web/app/sa/page.tsx
@@ -4,8 +4,18 @@ import { test } from './actions'
 export default function ServerAction() {
   return <div>
     <button onClick={async () => {
+      const start = new Date().getTime()
       const r = await test()
-      alert(JSON.stringify(r))
+      const end = new Date().getTime()
+      console.log(end - start, r)
     }}>Fire Server Action</button>
+
+    <button onClick={async () => {
+      const start = new Date().getTime()
+      const r = await fetch('/api/hello')
+      const d = await r.json()
+      const end = new Date().getTime()
+      console.log(end - start, d)
+    }}>Fire Regular fetch</button>
   </div>
 }
