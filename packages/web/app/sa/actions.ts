604ef05723a95fcf62376fe072218253b8e9d62d
Khue Nguyen <khuezy.nguyen@gmail.com>
2023-07-31 13:32:34 -0700

    add logs to server actions

--- packages/web/app/sa/actions.ts
+++ packages/web/app/sa/actions.ts
@@ -2,13 +2,9 @@
 
 export async function test() {
   const bacon = await fetch(
-    "http://worldtimeapi.org/api/timezone/America/Los_Angeles",
-    {
-      next: {
-        tags: ["bacon"],
-      },
-    }
+    "http://worldtimeapi.org/api/timezone/America/Los_Angeles"
   );
   const data = await bacon.json();
+  console.log(data);
   return { hello: "worlds", data };
 }
